0a61de3ffa1c68be524d8ec278ae46d918cd105e
Imfinebruno <i.bruno2000@gmail.com>
2022-08-23 17:34:31 -0300

    menu-bar-animado

--- script.js
+++ script.js
@@ -54,3 +54,47 @@ function smoothScrollTo(endX, endY, duration) {
       window.scroll(newX, newY);
     }, 1000 / 60);
   }
+
+
+class MobileNavBar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+  animateLinks(){
+    this.navLinks.forEach((link, index)=>{
+      link.style.animation
+      ? (link.style.animation = " ")
+      : (link.style.animation = `navLinkFade 0.5s ease backwards 0.3s`);
+    })
+  }
+  handleClick () {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent(){
+    this.mobileMenu.addEventListener('click', this.handleClick);
+  }
+
+  init(){
+    if(this.mobileMenu){
+      this.addClickEvent();
+    } return this
+  }
+}
+
+const mobileNavBar = new MobileNavBar(
+  ".mobile-menu",
+  ".nav",
+  ".nav li"
+);
+
+mobileNavBar.init();
